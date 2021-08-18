5a27c278f64a57d13fd4aaf47623676a8c552f62
2msalem <msalem@smartchargetech.com>
2021-08-18 18:27:06 +0300

    add-header-timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,53 @@
 import { Component, OnInit } from '@angular/core';
-import { PrimeNGConfig } from 'primeng/api';
+import { MenuItem, PrimeNGConfig } from 'primeng/api';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
-  title = 'front-end';
+export class AppComponent implements OnInit {
+	title = 'rest';
+	isLoaded = false;
+	items: MenuItem[] = [];
 
-  constructor(private primengConfig: PrimeNGConfig) {}
-  ngOnInit(): void {
-    this.primengConfig.ripple = true;
-  }
+	constructor() {
+		setTimeout(() => {
+			this.isLoaded = true;
+		}, 5000);
+	}
 
+	ngOnInit(): void {
+		this.items = [
+			{
+				label: 'File',
+				items: [
+					{
+						label: 'New',
+						icon: 'pi pi-fw pi-plus',
+						items: [
+							{
+								label: 'Project',
+								command(event) {
+									console.log('called');
+									console.log(event);
+								},
+							},
+							{ label: 'Other' },
+						],
+					},
+					{ label: 'Back' },
+					{ label: 'Quit' },
+				],
+			},
+			{
+				label: 'Edit',
+				icon: 'pi pi-fw pi-pencil',
+				items: [
+					{ label: 'Delete', icon: 'pi pi-fw pi-trash' },
+					{ label: 'Refresh', icon: 'pi pi-fw pi-refresh' },
+				],
+			},
+		];
+	}
 }
